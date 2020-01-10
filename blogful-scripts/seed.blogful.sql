3bf15bcd8ccd3da18ec1c22beb3e076d46e9b508
Patrick Gustafson <patrickgus@charter.net>
2020-01-09 21:11:14 -0800

    create blogful scripts

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,23 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+  ('Life', now() - '29 days'::INTERVAL, 'It''s a trip.'),
+  ('Hope', now() - '2 days'::INTERVAL, 'Never give it up.'),
+  ('Giving Back', now() - '9 days'::INTERVAL, 'Always a good thing to do.'),
+  ('Death', now() - '20 days'::INTERVAL, 'It is inevitable, so why be afraid?'),
+  ('Lose Weight Fast!', now() - '6 days'::INTERVAL, 'Just stop eating!'),
+  ('Baseball', now() - '12 days'::INTERVAL, 'The greatest sport on Earth.'),
+  ('Football', now() - '9 days'::INTERVAL, 'Which one?'),
+  ('Sinatra', now() - '3 days'::INTERVAL, 'The blue-eyed crooner.'),
+  ('Hear the Music', now() - '15 days'::INTERVAL, 'Feel it. Touch it. Taste it.'),
+  ('Feelings', now() - '22 days'::INTERVAL, 'Do not hide them.'),
+  ('Fear and Loathing', now() - '19 days'::INTERVAL, 'What is this muck? There''s no footing at all.'),
+  ('Subliminal Messages', now() - '7 days'::INTERVAL, 'Uoy ees I.'),
+  ('The Good, the Bad, and the Ugly', now() - '8 days'::INTERVAL, 'This town ain''t big enough for the three of us.'),
+  ('Pulp Fiction', now() - '11 days'::INTERVAL, 'Say what again, I dare you!'),
+  ('The 80s', now() - '1 days'::INTERVAL, 'The fast times of John Hughes'' movies.'),
+  ('PUBG', now() - '13 days'::INTERVAL, 'Anything to get that chicken dinner.'),
+  ('Star Wars', now() - '28 days'::INTERVAL, 'Episode 9 was great. No debate.'),
+  ('Honda', now() - '27 days'::INTERVAL, 'The most reliable car.'),
+  ('The Witcher', now() - '16 days'::INTERVAL, 'Toss a coin to your Witcher!'),
+  ('Help', now() - '5 days'::INTERVAL, 'I ran out of ideas.')
+;
